efbde580d5c366087e04c4d94103bf1685be18eb
Rhys-011003 <rod.rhys20031@gmail.com>
2022-09-16 21:56:40 +0530

    stuff

--- Scripts/darkmode.js
+++ Scripts/darkmode.js
@@ -1,11 +1,22 @@
+const  DKMDBtn=document.getElementById('Darkmode_button');
 function darkmode(){
-    DKMDBtn=document.getElementById('Darkmode_button');
+    DKMDBtn.classList.remove('rotate');
+    void DKMDBtn.offsetWidth;
+    DKMDBtn.classList.add('rotate');
+    
     if(DKMDBtn.textContent.includes("light_mode")){
+  
         DKMDBtn.textContent="dark_mode" ;
     }else if(DKMDBtn.textContent.includes("dark_mode")){
+ 
         DKMDBtn.textContent="light" ;
     }else if(DKMDBtn.textContent.includes("light")){
+        
         DKMDBtn.textContent="light_mode" ;
     }
     console.log(DKMDBtn.textContent)
+}
+
+function animate(){
+        
 }
