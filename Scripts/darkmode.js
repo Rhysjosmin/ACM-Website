2b9eb5692d50183e3564b7855c11cade2b076c8e
Rhys-011003 <rod.rhys20031@gmail.com>
2022-09-16 21:21:55 +0530

    removed files

--- Scripts/darkmode.js
+++ Scripts/darkmode.js
@@ -1,3 +1,11 @@
-function darkmode(DKMDBtn){
-    DKMDBtn.textContent="dark_mode"
+function darkmode(){
+    DKMDBtn=document.getElementById('Darkmode_button');
+    if(DKMDBtn.textContent.includes("light_mode")){
+        DKMDBtn.textContent="dark_mode" ;
+    }else if(DKMDBtn.textContent.includes("dark_mode")){
+        DKMDBtn.textContent="light" ;
+    }else if(DKMDBtn.textContent.includes("light")){
+        DKMDBtn.textContent="light_mode" ;
+    }
+    console.log(DKMDBtn.textContent)
 }
