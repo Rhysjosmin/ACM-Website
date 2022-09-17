1f95ece438c3511a7e291d27fc768221a7c27cbc
Rhys-011003 <rod.rhys20031@gmail.com>
2022-09-17 09:31:52 +0530

    added slideshow

---
+++ Scripts/Changeimg.js
@@ -0,0 +1,17 @@
+setInterval(()=>changeImg(),4000);
+const img =document.getElementById('Heroimg');
+let i=2;
+const MAXIMG=9
+function changeImg(){
+    img.classList.remove('ani1');
+    void img.offsetWidth;
+    img.classList.add('ani1');
+    
+    img.src='./media/Img/Img_'+i.toString().concat(".jpg")
+    i++;
+
+ 
+    if(i==MAXIMG){
+        i=1;
+    }
+}
