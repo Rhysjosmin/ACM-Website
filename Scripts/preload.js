628c3e0a884f73d3afd905e60e012b09157b63f2
Rhys-011003 <rod.rhys20031@gmail.com>
2022-09-23 03:14:41 +0530

    cleaned up

---
+++ Scripts/preload.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',()=>{
+    Loaded()
+    
+});
+
+
+
+
+
+function Loaded(){
+    const preload=document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+}
+
+
+
+
