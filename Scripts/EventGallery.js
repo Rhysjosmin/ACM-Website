ce5d5e95846e6a200074dba3aa505aadfed097b2
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-11-23 16:49:07 +0530

    Stuff

---
+++ Scripts/EventGallery.js
@@ -0,0 +1,13 @@
+// var cntWd, sldWd, tb;
+// $(function () {
+//   cntWd = $("#EventReports").innerWidth();
+//   tb = $("#EventReports");
+//   sldWd = tb.outerWidth();
+
+//   $("#EventReports").mousemove(function (e) {
+//     alert('Hi')
+//     tb.css({
+//       left: ((cntWd - sldWd) * (e.pageX / cntWd).toFixed(3)).toFixed(1) + "px"
+//     });
+//   });
+// });
