4c7daad5b1c51f6ed6c7a800c83c549811e467bb
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-12-11 14:17:54 +0530

    Update Changeimg.js

--- Scripts/Changeimg.js
+++ Scripts/Changeimg.js
@@ -44,45 +44,56 @@ function PrevImg(){
 
 const Hero=document.getElementById('Hero')
 
-Hero.onmousedown=e=>{
-    Hero.dataset.mouseDownAt=e.clientX;
+// Hero.onmousedown=e=>{
+//     Hero.dataset.mouseDownAt=e.clientX;
 
-}
+// }
 
-Hero.onmousemove=e=>{
-    if(Hero.dataset.mouseDownAt==="0") return;
-    const mouseDelta = parseFloat(Hero.dataset.mouseDownAt)-e.clientX,
-        maxDelta=window.innerWidth/2;
+// Hero.onmousemove=e=>{
+//     if(Hero.dataset.mouseDownAt==="0") return;
+//     const mouseDelta = parseFloat(Hero.dataset.mouseDownAt)-e.clientX,
+//         maxDelta=window.innerWidth/2;
 
-    const percentage =Math.abs(Math.round((mouseDelta/maxDelta)*(MAXIMG-MINIMG)))
-    if(MINIMG<percentage<(MAXIMG)){
-        i=percentage
-    }else{
-        i=MINIMG
-    }
-    console.log(percentage)
-    if(percentage>0) NextImg(); else PrevImg();
-}
-Hero.onmouseup=()=>{
-    Hero.dataset.mouseDownAt="0"
-}
+//     const percentage =Math.abs(Math.round((mouseDelta/maxDelta)*(MAXIMG-MINIMG)))
+//     if(MINIMG<percentage<(MAXIMG)){
+//         i=percentage
+//     }else{
+//         i=MINIMG
+//     }
+//     console.log(percentage)
+//     if(percentage>0) NextImg(); else PrevImg();
+// }
+// Hero.onmouseup=()=>{
+//     Hero.dataset.mouseDownAt="0"
+// }
 
 
 Hero.ontouchstart=e=>{
-    Hero.dataset.mouseDownAt=e.clientX;
+    Hero.dataset.mouseDownAt=e.changedTouches[0].clientX
     // console.log(percentage)
 
 }
 
-Hero.ontouchmove=e=>{
+// Hero.ontouchmove=e=>{
+//     // if(Hero.dataset.mouseDownAt==="0") return;
+//     // const mouseDelta = parseFloat(Hero.dataset.mouseDownAt)-e.clientX,
+//     //     maxDelta=window.innerWidth/2;
+
+//     // const percentage =(mouseDelta/maxDelta)*100
+//     // console.log(percentage)
+//     // if(percentage>0) NextImg(); else PrevImg();
+// }
+Hero.ontouchend=e=>{
+    // console.log(e.changedTouches[0].clientX)
     if(Hero.dataset.mouseDownAt==="0") return;
-    const mouseDelta = parseFloat(Hero.dataset.mouseDownAt)-e.clientX,
+    const mouseDelta = parseFloat(Hero.dataset.mouseDownAt)-e.changedTouches[0].clientX
         maxDelta=window.innerWidth/2;
 
-    const percentage =(mouseDelta/maxDelta)*100
+    const percentage =(mouseDelta/maxDelta)
+
     console.log(percentage)
+
     if(percentage>0) NextImg(); else PrevImg();
-}
-Hero.ontouchend=()=>{
     Hero.dataset.mouseDownAt="0"
+    
 }
