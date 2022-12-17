63b32d01a0ae5437722f6811df42a3cf89875520
Rhys-011003 <55994011+Rhys-011003@users.noreply.github.com>
2022-12-17 13:08:36 +0530

    Clean Up

--- Scripts/JsonContent.js
+++ Scripts/JsonContent.js
@@ -1,56 +1,77 @@
 
 
-async function loadProject(){
+async function loadProject1(){
     const {default:project}= await import('../Json/News.json',{
         assert:{
             type:"json"
         }
     })
-    AddProject(project)
+    AddNEWS(project,'FEEDITEMTEMPLATE','feed');
+
 }
 
-loadProject()
+async function loadProject2(){
 
-function AddProject(project){
-    
+    const {default:Upcoming}= await import('../Json/UpcomingEvents.json',{
+        assert:{
+            type:"json"
+        }
+    })
+    AddUpcoming(Upcoming,"UpcommingEventTemplate",'announcments');
+}
+loadProject2()
+loadProject1()
 
-    
+function AddNEWS(project,templateDOM,DestinationDOM){
 
-    const template=document.getElementById('FEEDITEMTEMPLATE')
-    const container=document.getElementById('feed')
-    
-
-   
+    const template=document.getElementById(templateDOM)
+    const container=document.getElementById(DestinationDOM)
+       
     for(var i=0;i<1000;i++)
  {
 
     let Name=project.News[i].Title
     let Description=project.News[i].Description
     let Link=project.News[i].Link
-
-
-    
-  
-
-
     content = template.content.cloneNode(true) // the true is for deep cloning
 
     content.getElementById('FeedTitle').textContent=Name
     content.getElementById('FeedText').textContent=Description
    content.getElementById('Link').href=Link
-
-
-
-   
-   
-  
-
-   
-
-
     container.appendChild(content)
+ 
+ }
+ 
+}
 
+
+function AddUpcoming(project,templateDOM,DestinationDOM){
+
+    const template=document.getElementById(templateDOM)
+    const container=document.getElementById(DestinationDOM)
+       
+    for(var i=0;i<1000;i++)
+ {
+
+    let Title=project.Upcomming[i].Title
+    let Presenter=project.Upcomming[i].Presenter
+    let Date=project.Upcomming[i].Date
+    let Description=project.Upcomming[i].Description
+    let Register_Link=project.Upcomming[i].Register_Link
+    let ReadMore=project.Upcomming[i].ReadMore
+    content = template.content.cloneNode(true) // the true is for deep cloning
+
+    content.getElementById('TITLE').textContent=Title
+    content.getElementById('PRESENTER').textContent=Presenter
+    content.getElementById('Timing').textContent=Date
     
+    content.getElementById('Description').textContent=Description
+    
+    content.getElementById('RegisterButton').href=Register_Link
+    content.getElementById('ReadMoreButton').href=ReadMore
+    
+    container.appendChild(content)
+ 
  }
  
 }
