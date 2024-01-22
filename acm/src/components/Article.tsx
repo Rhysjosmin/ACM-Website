935ac76249b41f7b708bea3b6f81c478674829d8
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-01-22 19:26:42 +0530

    add loading state

--- acm/src/components/Article.tsx
+++ acm/src/components/Article.tsx
@@ -1,20 +1,26 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { Space_Grotesk } from "next/font/google";
 import { MaterialSymbolsLightOpenInNewRounded } from "./icons";
+import { useState } from "react";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export default function Article(props: any) {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <Link href={props.address}>
       {/* <div className="aspect-[5/3.5] w-[calc(100vw-2.5rem)] md:w-[100%] md:h-[26rem]  rounded-lg md:rounded-2xl relative overflow-hidden"> */}
       <div className="relative flex flex-col w-full h-full border rounded-lg border-white/10 group hover:border-white/30 md:rounded-2xl overflow-hidden">
         <Image
+          onLoad={() => setImageLoaded(true)}
           height={800}
           width={800}
           src={props.image}
-          className="object-cover w-full h-auto aspect-video"
+          className={`${
+            imageLoaded ? "opacity-100 " : "opacity-0 "
+          }  transition-opacity h-auto duration-500 object-cover w-full aspect-video`}
           alt="Article Image"
         />
 
